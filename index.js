5c5701b485831705f5cf2848682dbf27dba1d446
tondajiras <antonin.jiras@gmail.com>
2024-04-12 14:06:03 +0200

    nic

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const grid = document.getElementById("grid");
 
-const GridList = ["blue", "green", "red", "yellow", "orange", "grey"]
-let change = "nic";
 
 setInterval(function() {
-    change = GridList[0];
-    GridList[0] = GridList[1];
-    GridList[1] = GridList[2];
-    GridList[2] = GridList[3];
-    GridList[3] = GridList[4];
-    GridList[4] = GridList[5];
-    GridList[5] = change;
-    grid.style.gridTemplateAreas = `"${GridList[0]} ${GridList[1]} ${GridList[2]}"
-                                "${GridList[3]} ${GridList[4]} ${GridList[5]}"`;
+    let order = grid.style.order;
+    grid.style.order = Math.floor(Math.random() * 6);
+
+
 }, 1000);
